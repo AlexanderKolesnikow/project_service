bbdf174f6fa18ea41c752d6fa599cc4ef99c3c13
Alexander Kolesnikov <125891795+AlexanderKolesnikow@users.noreply.github.com>
2023-10-18 15:21:06 +0100

    Added FileService for working with cloud storage

---
+++ src/main/java/com/kite/kolesnikov/project_service/exception/FileUploadException.java
@@ -0,0 +1,7 @@
+package com.kite.kolesnikov.project_service.exception;
+
+public class FileUploadException extends RuntimeException{
+    public FileUploadException(String message){
+        super(message);
+    }
+}
