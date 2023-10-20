db52499545f5856d2ae59efa4177a1747a2119c1
Alexander Kolesnikov <125891795+AlexanderKolesnikow@users.noreply.github.com>
2023-10-20 17:50:14 +0200

    Pass the user ID through the request header instead of the user context to be able to use it through Swagger

--- src/main/java/com/kite/kolesnikov/projectservice/controller/ProjectResourceController.java
+++ src/main/java/com/kite/kolesnikov/projectservice/controller/ProjectResourceController.java
@@ -1,11 +1,12 @@
 package com.kite.kolesnikov.projectservice.controller;
 
-import com.kite.kolesnikov.projectservice.config.context.UserContext;
 import com.kite.kolesnikov.projectservice.dto.resource.GetResourceDto;
 import com.kite.kolesnikov.projectservice.dto.resource.ResourceDto;
 import com.kite.kolesnikov.projectservice.dto.resource.UpdateResourceDto;
 import com.kite.kolesnikov.projectservice.service.ProjectResourceService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -15,15 +16,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -32,15 +25,15 @@ import org.springframework.web.multipart.MultipartFile;
 @Tag(name = "Project Resource controller")
 public class ProjectResourceController {
     private final ProjectResourceService projectFileService;
-    private final UserContext userContext;
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
     @Operation(summary = "Upload a file",
             description = "Uploads a file to the cloud object storage and saves metadata to the database.")
     public ResourceDto uploadFile(@RequestParam("file") MultipartFile multipartFile,
-                                  @RequestParam long projectId) {
-        long userId = userContext.getUserId();
+                                  @RequestParam long projectId,
+                                  @Parameter(description = "User ID", required = true, in = ParameterIn.HEADER)
+                                  @RequestHeader("x-user-id") Long userId) {
 
         return projectFileService.uploadFile(multipartFile, projectId, userId);
     }
@@ -50,8 +43,9 @@ public class ProjectResourceController {
             summary = "Update a file",
             description = "Updates a file in the cloud object storage and refreshes the metadata in the database.")
     public UpdateResourceDto updateFile(@RequestParam("file") MultipartFile multipartFile,
-                                        @PathVariable long resourceId) {
-        long userId = userContext.getUserId();
+                                        @PathVariable long resourceId,
+                                        @Parameter(description = "User ID", required = true, in = ParameterIn.HEADER)
+                                        @RequestHeader("x-user-id") Long userId) {
 
         return projectFileService.updateFile(multipartFile, resourceId, userId);
     }
@@ -68,8 +62,10 @@ public class ProjectResourceController {
                     @ApiResponse(responseCode = "404", description = "The file does not exist.")
             }
     )
-    public ResponseEntity<InputStreamResource> downloadFile(@PathVariable long resourceId) {
-        long userId = userContext.getUserId();
+    public ResponseEntity<InputStreamResource> downloadFile(@PathVariable long resourceId,
+                                                            @Parameter(description = "User ID", required = true, in = ParameterIn.HEADER)
+                                                            @RequestHeader("x-user-id") Long userId) {
+
         GetResourceDto resourceDto = projectFileService.getFile(resourceId, userId);
         InputStreamResource inputStreamResource = new InputStreamResource(resourceDto.getInputStream());
 
@@ -87,8 +83,9 @@ public class ProjectResourceController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @Operation(summary = "Delete a file",
             description = "Deletes a file from the storage and its metadata from the database.")
-    public void deleteFile(@PathVariable long resourceId) {
-        long userId = userContext.getUserId();
+    public void deleteFile(@PathVariable long resourceId,
+                           @Parameter(description = "User ID", required = true, in = ParameterIn.HEADER)
+                           @RequestHeader("x-user-id") Long userId) {
 
         projectFileService.deleteFile(resourceId, userId);
     }
